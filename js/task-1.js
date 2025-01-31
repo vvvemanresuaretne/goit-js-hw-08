5af0428a1ad612f2ed949f50003843ef32335412
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-01-31 11:47:13 +0100

    update

--- js/task-1.js
+++ js/task-1.js
@@ -6,8 +6,8 @@ function  makeTransaction(quantity, pricePerDroid, customerCredits){
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   }
 }
-console.log(makeTransaction(5, 300, 2000)); // "You ordered 5 droids worth 1500 credits!"
-console.log(makeTransaction(10, 500, 4000)); // "Insufficient funds!"
-console.log(makeTransaction(3, 100, 500)); // "You ordered 3 droids worth 300 credits!"
-console.log(makeTransaction(7, 200, 1400)); // "You ordered 7 droids worth 1400 credits!"
-console.log(makeTransaction(1, 1000, 500)); // "Insufficient funds!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
