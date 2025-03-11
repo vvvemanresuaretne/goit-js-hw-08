04cb5284525844d82afe70d2c0a035471b1cf684
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-03-11 15:47:21 +0100

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,29 +1,9 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+const categoriesList = document.querySelector('#categories');
+const categoryItems = categoriesList.querySelectorAll('.item');
+console.log(`Кількість категорій: ${categoryItems.length}`);
+categoryItems.forEach(item => {
+    const title = item.querySelector('h2').textContent;
+    const elementsCount = item.querySelectorAll('ul li').length;
+    console.log(`Категорія: ${title}`);
+    console.log(`Кількість елементів: ${elementsCount}`);
+});
--- js/task-3.js
+++ js/task-3.js
@@ -1,27 +1,5 @@
-class StringBuilder {
-  #value;
- constructor(initialValue) {
-    this.#value = initialValue;
-  }
- getValue() {
-    return this.#value;
-  }
- padEnd(str) {
-    this.#value += str;
-  }
- padStart(str) {
-    this.#value = str + this.#value;
-  }
- padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
-  }
-}
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+ document.getElementById("name-input").addEventListener("input", function () {
+    const input = this.value.trim(); 
+    document.getElementById("name-output").textContent = input || "Anonymous"; 
+  });
+
