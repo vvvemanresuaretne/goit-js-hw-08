91ec649c4c5baade0fb2f5adf0a5c08bbf16b7b1
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-04-11 08:05:11 +0200

    update

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const categoriesList = document.querySelector('#categories');
-const categoryItems = categoriesList.querySelectorAll('.item');
-console.log(`Кількість категорій: ${categoryItems.length}`);
-categoryItems.forEach(item => {
-    const title = item.querySelector('h2').textContent;
-    const elementsCount = item.querySelectorAll('ul li').length;
-    console.log(`Категорія: ${title}`);
-    console.log(`Кількість елементів: ${elementsCount}`);
-});
--- js/task-3.js
+++
@@ -1,5 +0,0 @@
- document.getElementById("name-input").addEventListener("input", function () {
-    const input = this.value.trim(); 
-    document.getElementById("name-output").textContent = input || "Anonymous"; 
-  });
-
